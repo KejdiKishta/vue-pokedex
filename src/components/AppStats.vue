9197f07aa456b4928129df70c41991bd2aec2d07
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-09-15 17:32:33 +0200

    fix: stats style

--- src/components/AppStats.vue
+++ src/components/AppStats.vue
@@ -23,10 +23,14 @@ export default {
 
 <template>
     <div class="p-2 pt-0">
-        <div class="bg-success p-3" style="height: 200px;">
+        <div class="bg-success rounded p-3" style="height: 200px;">
+            <h5 class="text-center text-uppercase fw-bold">{{ this.store.info.name }}</h5>
             <div v-for="item in getStats" class="d-flex">
                 <div class="w-50">{{ item.stat.name }}</div>
-                <div class="w-50">{{ item.base_stat }}</div>
+                <!-- <div class="w-50">{{ item.base_stat }}</div> -->
+                <div class="bg-secondary-subtle w-50 rounded align-self-center" style="height: 5px;">
+                    <div class="h-100 bg-black rounded" :style="{width: item.base_stat / 2 + '%' }"></div>
+                </div>
             </div>
         </div>
     </div>
